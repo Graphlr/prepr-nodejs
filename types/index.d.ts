acbdb1d14598c7fa2882b27ebc85f438607a5c89
dannybevers <danny.bevers@live.nl>
2020-12-17 23:50:55 +0100

    Add userId to typescript

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,6 +5,6 @@ type CallbackFn<T = unknown> = (err: Error | null, res: T | null) => void;
 export interface Prepr {
 }
 
-declare function prepr(accessToken: string, timeout?: number): Prepr;
+declare function prepr(accessToken: string, timeout?: number, userId?: string): Prepr;
 
 export default prepr;
